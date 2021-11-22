b1744922839fff47c28c1780baddf8ce7208def2
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2021-11-21 18:16:59 -0600

    Update FindMinInRotatedSortedArray.java

--- FindMinInRotatedSortedArray.java
+++ FindMinInRotatedSortedArray.java
@@ -13,33 +13,70 @@ class Solution {
         int low = 0;
         int high = nums.length - 1;
         
+        //explore as long as current sub array size is above 1
+        //if sub array size is 1, then that is the min element
         while(low < high)
         {
             int mid = low + (high - low) / 2;
-            int prev = (mid - 1 + nums.length) % nums.length;
-            int next = (mid + 1) % nums.length;
-            
-            if(nums[mid] <= nums[prev] && nums[mid] <= nums[next]) //mid is smaller than both prev and next, return mid
+            if(nums[mid] > nums[high])
             {
-                return nums[mid];
-            }
-            
-            if(nums[low] <= nums[high]) //full array sorted
-            {
-                return nums[low];
-            }
-            
-            if(nums[low] <= nums[mid]) //left half sorted
-            {
+                //eg: [4,5,6,7,0,1,2]
+                //we are at the first increasing chunk
+                //all the elements to left of mid and mid are rotated, so min is at the right half
                 low = mid + 1;
             }
-            
-            else //right half sorted
+            else if(nums[mid] < nums[high]) //else -> not <= as mid and high can't be same within while 
             {
-                high = mid - 1;
+                //eg: [6,5,0,1,2,3,4]
+                //the rotation has happened in the left half and we are currently in the second increasing chunk
+                //potentially this mid could be min
+                high = mid;
             }
         }
         
-        return nums[low];
+        return nums[high]; //nums[low]
+    }
+}
+
+// 154.
+// time -> avg: O(log n), worst: O(n)
+class Solution {
+    public int findMin(int[] nums) {
+        //edge
+        if(nums == null || nums.length == 0)
+        {
+            return -1;
+        }
+        
+        int low = 0;
+        int high = nums.length - 1;
+        
+        //explore as long as current sub array size is above 1
+        //if sub array size is 1, then that is the min element
+        while(low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] > nums[high])
+            {
+                //eg: [4,5,6,7,0,1,2]
+                //we are at the first increasing chunk
+                //all the elements to left of mid and mid are rotated, so min is at the right half
+                low = mid + 1;
+            }
+            else if(nums[mid] < nums[high])
+            {
+                //eg: [6,5,0,1,2,3,4]
+                //the rotation has happened in the left half and we are currently in the second increasing chunk
+                //potentially this mid could be min
+                high = mid;
+            }
+            else
+            {
+                //eg: 3,1,3,3,3
+                high--;
+            }
+        }
+        
+        return nums[high]; //nums[low]
     }
 }
