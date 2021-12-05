2852fc513cea5577dec15f5c440eb6c9f05a84a1
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2021-12-05 12:47:51 -0600

    Update SearchInRotatedSortedArray.java

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -1,48 +1,105 @@
 // 33.
 // time - O(log n)
-// space - O(1)
-
 class Solution {
     public int search(int[] nums, int target) {
         //edge
         if(nums == null || nums.length == 0)
         {
-            return -1;
+            return -1; //not found
         }
         
         int low = 0;
         int high = nums.length - 1;
-        
         while(low <= high)
         {
             int mid = low + (high - low) / 2;
             if(nums[mid] == target)
             {
-                return mid;
+                return mid; //target found - return
             }
-            if(nums[low] <= nums[mid]) //left half is sorted
+            //check if left half is sorted eg: [5,6,7,8,9,1,2]
+            else if(nums[low] <= nums[mid]) //equality condition to handle case when mid and low are same
             {
-                if(nums[low] <= target && target < nums[mid]) //target is present in this half
+                if(nums[low] <= target && target < nums[mid]) //target is in the left half which is sorted
                 {
-                    high = mid - 1;
+                    high = mid - 1; 
                 }
                 else
                 {
-                    low = mid + 1;
+                    low = mid + 1; //go to right half which is a smaller rotated sorted array
                 }
             }
-            else //right half is sorted
+            else //right half is sorted eg: [5,0,1,2,3]
             {
-                if(nums[mid] < target && target <= nums[high]) //target is present in this half
+                if(nums[mid] < target && target <= nums[high]) //target is in right sorted half
                 {
                     low = mid + 1;
                 }
                 else
                 {
-                    high = mid - 1;
+                    high = mid - 1; //go to left half which is a smaller rotated sorted array
                 }
-            }      
+            }
         }
-        return -1;
+        
+        return -1; //not found
+    }
+}
+
+// 81.
+// time - O(log n)
+class Solution {
+    public boolean search(int[] nums, int target) {
+        //edge
+        if(nums == null || nums.length == 0)
+        {
+            return false; //not found
+        }
+        
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target)
+            {
+                return true; //target found - return
+            }
+            //eg: [3,1,3,3,3] - can't decide on which half is sorted
+            while(low < mid && nums[low] == nums[mid])
+            {
+                low++;
+            }
+            while(mid < high && nums[mid] == nums[high])
+            {
+                high--;
+            }
+            
+            //check if left half is sorted eg: [5,6,7,8,9,1,2]
+            if(nums[low] <= nums[mid]) //equality condition to handle case when mid and low are same
+            {
+                if(nums[low] <= target && target < nums[mid]) //target is in the left half which is sorted
+                {
+                    high = mid - 1; 
+                }
+                else
+                {
+                    low = mid + 1; //go to right half which is a smaller rotated sorted array
+                }
+            }
+            else //right half is sorted eg: [5,0,1,2,3]
+            {
+                if(nums[mid] < target && target <= nums[high]) //target is in right sorted half
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1; //go to left half which is a smaller rotated sorted array
+                }
+            }
+        }
+        
+        return false; //not found
     }
 }
