e7854ef3f1bc0fa9ea4221325b98ff629adcf463
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2021-12-05 00:43:59 -0600

    Update FindPeak.java

--- FindPeak.java
+++ FindPeak.java
@@ -38,3 +38,42 @@ class Solution {
         return -1;
     }
 }
+
+// 852.
+// time - O(log n)
+// find in a bitonic array -> find peak -> do BS in left half for target -> if not found do BS in right half
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        //edge
+        if(arr == null || arr.length == 0)
+        {
+            return -1; //invalid case
+        }
+        
+        int low = 0; //initial search space is the whole array
+        int high = arr.length - 1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            //mid is peak if nums[mid - 1] <= nums[mid] <= nums[mid + 1], equality condition not needed due to no duplicates
+            if((mid == 0 || arr[mid] >= arr[mid - 1]) && (mid == arr.length - 1 || arr[mid] >= arr[mid + 1]))
+            {
+                return mid;
+            }
+            //current mid is less than next, so currently in increasing half, peak is in [mid + 1, high]
+            else if(mid != arr.length - 1 && arr[mid] < arr[mid + 1])
+            {
+                low = mid + 1;
+            }
+            //current mid is less than prev, currently in decreasing half and mid is not peak as first if failed
+            //peak is [low, mid - 1]
+            else if(mid != 0 && arr[mid] < arr[mid - 1])
+            {
+                high = mid - 1;
+            }
+        }
+        
+        return -1; //code never reaches here
+    }
+}
