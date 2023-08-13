70919aed2af8ebd5b3012a46a80ed43bfc9e1d0c
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-08-13 18:17:30 -0500

    Update FindShipCapacity.java

--- FindShipCapacity.java
+++ FindShipCapacity.java
@@ -1,64 +1,71 @@
 // 1011.
-// time - O(n * log[sum of all elements - max element]) with constant space
+// time - O(log(sum of weights of all packages - weight of largest package) * number of packages)
+// space - constant
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
         //edge
+        if(days <= 0)
+        {
+            return Integer.MAX_VALUE; //if threshold days is 0 or less, no package can be shipped with any capacity
+        }
         if(weights == null || weights.length == 0)
         {
-            return 0;
+            return 0; //no packages, so ship is not needed
         }
-        
-        //observation - min weight of the ship = max element in the array
-        //max weight of ship = sum of all elements in array - ships in 1 day taking all elements at once
-        //brute force - go from low to high sequentially, check if the days is needed is less than target
-        int low = Integer.MIN_VALUE;
-        int high = 0;
-        for(int num : weights)
+
+        //every package must be shipped, so capacity of ship should atleast be weight of heaviest pacakage
+        //with this capacity, each package will be shipped on its own day, total days needed = number of packages
+        //if all packages have to be shipped on same day, the ship's capacity should be atleast sum of weights of all packages
+        int minCapacity = Integer.MIN_VALUE; //weight of largest package should be set
+        int maxCapacity = 0; //sum of weights of all packages should be set
+        for(int weight : weights)
         {
-            low = Math.max(low, num);
-            high += num;
+            minCapacity = Math.max(minCapacity, weight);
+            maxCapacity += weight;
         }
-        int result = 0;
-        
-        while(low <= high)
+
+        //optimal capacity should be in [minCapacity, maxCapacity]
+        int result = maxCapacity; //minize result within [min, max] so intialize result to max
+        while(minCapacity <= maxCapacity)
         {
-            int mid = low + (high - low) / 2;
-            int daysNeeded = findDaysGivenCapacity(weights, mid); //days needed if min is the capacity
+            int mid = minCapacity + (maxCapacity - minCapacity) / 2;
+            //compute days needed if mid is ship's capacity
+            int daysNeeded = findDays(weights, mid);
+
+            //if days needed with mid is lesser than d
             if(daysNeeded <= days)
             {
-                //if shipment can be done within given days, update result and try reducing capacity by moving to left
-                result = mid;
-                high = mid - 1;
+                result = mid; //update result and try seraching with even lower capacities in left
+                maxCapacity = mid - 1;
             }
             else
             {
-                //shipment takes more days
-                //try increasing capacity by moving to right
-                low = mid + 1;
+                minCapacity = mid + 1; //days needed exceeds threshold, increase capacity by searching in right to reduce days
             }
         }
-        
+
         return result;
     }
-    
-    //util method to find days needed to ship all packages given capacity
-    //time - O(n)
-    private int findDaysGivenCapacity(int[] weights, int capacity)
+
+    //computes days needed to ship all packages with given ship's capacity
+    //time - O(n) with constant space
+    private int findDays(int[] packages, int capacity)
     {
-        int currentWeight = 0;
         int days = 1;
-        for(int num : weights)
+        int currentWeight = 0; //initially ship is empty
+        for(int weight : packages)
         {
-            if(num + currentWeight > capacity)
+            if(currentWeight + weight <= capacity)//current package can be included in current day
             {
-                days++; //increase days needed by 1
-                currentWeight = num; //the next day current weight becomes weight of current item
+                currentWeight += weight;
             }
             else
             {
-                currentWeight += num; //including current item in the same day
+                currentWeight = weight;
+                days++; //current package should be shipped on the next day
             }
         }
+
         return days;
     }
 }
