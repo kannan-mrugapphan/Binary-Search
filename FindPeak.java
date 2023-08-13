788d0b3336cb43c3fae4ea3926a135515d5e2ac9
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-08-13 12:15:36 -0500

    Update FindPeak.java

--- FindPeak.java
+++ FindPeak.java
@@ -1,78 +1,58 @@
 // 162.
-// time - O(log n) 
-//space - constant
-
+// time - O(log n)
 class Solution {
     public int findPeakElement(int[] nums) {
         //edge
         if(nums == null || nums.length == 0)
         {
-            return -1; //invalid case
+            return -1; 
         }
-        
-        int low = 0; //initial search space is the whole array
-        int high = nums.length - 1;
-        
-        while(low <= high)
+        //theoretically -infinity is present before element at 0th index and after element at last index
+        //array has only one element
+        if(nums.length == 1)
         {
-            int mid = low + (high - low) / 2;
-            //mid is peak if nums[mid - 1] <= nums[mid] <= nums[mid + 1]
-            if((mid == 0 || nums[mid] >= nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] >= nums[mid + 1]))
-            {
-                return mid;
-            }
-            //current mid is less than next, potentially mid + 1 is a peak as there is one smaller element in left
-            else if(mid != nums.length - 1 && nums[mid] < nums[mid + 1])
-            {
-                low = mid + 1;
-            }
-            //current mid is less than prev, potentially mid - 1 is a peak as there is one smaller element right
-            else if(mid != 0 && nums[mid] < nums[mid - 1])
-            {
-                high = mid - 1;
-            }
+            return 0;
+        }
+        //to avoid cases to handle array index out of bounds exception, initialize search space to [1,nums.length-2]
+        //check if 0th index and last index is a peak seperately
+        if(nums[0] > nums[1])
+        {
+            return 0;
+        }
+        if(nums[nums.length - 1] > nums[nums.length - 2])
+        {
+            return nums.length - 1;
         }
         
-        return -1; //code never reaches here
-    }
-}
 
-// 852.
-// time - O(log n)
-//same as above
-// find in a bitonic array -> find peak -> do BS in left half for target -> if not found do BS in right half
-class Solution {
-    public int peakIndexInMountainArray(int[] arr) {
-        //edge
-        if(arr == null || arr.length == 0)
-        {
-            return -1; //invalid case
-        }
-        
-        int low = 0; //initial search space is the whole array
-        int high = arr.length - 1;
-        
+        int low = 1;
+        int high = nums.length - 2;
+
+        //as long as search space has atleast 1 element
         while(low <= high)
         {
             int mid = low + (high - low) / 2;
-            //mid is peak if nums[mid - 1] <= nums[mid] <= nums[mid + 1], equality condition not needed due to no duplicates
-            if((mid == 0 || arr[mid] >= arr[mid - 1]) && (mid == arr.length - 1 || arr[mid] >= arr[mid + 1]))
+            //check if mid is a peak
+            if(nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
             {
                 return mid;
             }
-            //current mid is less than next, so currently in increasing half, peak is in [mid + 1, high]
-            else if(mid != arr.length - 1 && arr[mid] < arr[mid + 1])
+
+            //check if mid is part of decreasing half
+            if(nums[mid] > nums[mid + 1])
             {
-                low = mid + 1;
+                //mid is not a peak and is in decreasing half
+                high = mid - 1; //check for peak in left half
             }
-            //current mid is less than prev, currently in decreasing half and mid is not peak as first if failed
-            //peak is [low, mid - 1]
-            else if(mid != 0 && arr[mid] < arr[mid - 1])
+
+            //else mid is in increasing half
+            else if(nums[mid] < nums[mid + 1])
             {
-                high = mid - 1;
+                //mid is not a peak and is in increasing half
+                low = mid + 1; //check for peak in right half
             }
         }
-        
-        return -1; //code never reaches here
+
+        return -1; //peak doesn't exist
     }
 }
