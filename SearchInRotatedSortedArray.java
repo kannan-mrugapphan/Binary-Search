8fcfb629cbde5ccb7d5905e7777d2061f0046c3d
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-08-12 23:48:39 -0500

    Update SearchInRotatedSortedArray.java

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -1,105 +1,120 @@
-// 33.
-// time - O(log n)
+// 39.
+// time - O(log n) with constant space
 class Solution {
     public int search(int[] nums, int target) {
         //edge
         if(nums == null || nums.length == 0)
         {
-            return -1; //not found
-        }
-        
+            return -1; //nums[] is invalid, target absent
+        }    
+
+        //initially search space is the whole array
         int low = 0;
         int high = nums.length - 1;
+
+        //as long as search space has valid elements
         while(low <= high)
         {
             int mid = low + (high - low) / 2;
+            //check if mid index has the target
             if(nums[mid] == target)
             {
-                return mid; //target found - return
+                return mid;
             }
-            //check if left half is sorted eg: [5,6,7,8,9,1,2]
-            else if(nums[low] <= nums[mid]) //equality condition to handle case when mid and low are same
+
+            //identify the sorted half
+            if(nums[low] <= nums[mid]) // <= equal condition even though the array has unique elements to account for scenario when both low and mid are at same index
             {
-                if(nums[low] <= target && target < nums[mid]) //target is in the left half which is sorted
+                //left half is sorted, check if target is within left sorted half
+                if(nums[low] <= target && target < nums[mid])
                 {
-                    high = mid - 1; 
+                    high = mid - 1; //search in left sorted half
                 }
                 else
                 {
-                    low = mid + 1; //go to right half which is a smaller rotated sorted array
+                    low = mid + 1; //search in right unsorted half
                 }
             }
-            else //right half is sorted eg: [5,0,1,2,3]
+            else
             {
-                if(nums[mid] < target && target <= nums[high]) //target is in right sorted half
+                //right half is sorted, check if target is within right sorted half
+                if(nums[mid] < target && target <= nums[high])
                 {
-                    low = mid + 1;
+                    low = mid + 1; //search in right sorted half
                 }
                 else
                 {
-                    high = mid - 1; //go to left half which is a smaller rotated sorted array
+                    high = mid - 1; //search in left unsorted half
                 }
             }
         }
-        
-        return -1; //not found
+
+        return -1; //target absent
     }
 }
 
 // 81.
-// time - O(log n)
+// time - O(log n) in average case and O(n) in worst case for examples like [3,3,3,3,3] with target = 4
+// space - constant
 class Solution {
     public boolean search(int[] nums, int target) {
         //edge
         if(nums == null || nums.length == 0)
         {
-            return false; //not found
+            return false; //nums[] is invalid, target absent
         }
-        
+
+        //initially search space is whole array
         int low = 0;
         int high = nums.length - 1;
+
+        //as long as search space is valid
         while(low <= high)
         {
             int mid = low + (high - low) / 2;
+            //check if mid index has target
             if(nums[mid] == target)
             {
-                return true; //target found - return
-            }
-            //eg: [3,1,3,3,3] - can't decide on which half is sorted
-            while(low < mid && nums[low] == nums[mid])
-            {
-                low++;
-            }
-            while(mid < high && nums[mid] == nums[high])
-            {
-                high--;
+                return true;
             }
             
-            //check if left half is sorted eg: [5,6,7,8,9,1,2]
-            if(nums[low] <= nums[mid]) //equality condition to handle case when mid and low are same
+            //if it is not possible to identify the sorted half
+            //eg: [3,1,3,3,3] 
+            if(nums[low] == nums[mid] && nums[mid] == nums[high])
             {
-                if(nums[low] <= target && target < nums[mid]) //target is in the left half which is sorted
+                //just ignore low and high indices, and proceed to next iteration
+                low++;
+                high--;
+                continue;
+            }
+
+            //identify sorted half
+            if(nums[low] <= nums[mid])
+            {
+                //left half is sorted, check if target is within left half
+                if(nums[low] <= target && target < nums[mid])
                 {
                     high = mid - 1; 
                 }
                 else
                 {
-                    low = mid + 1; //go to right half which is a smaller rotated sorted array
+                    low = mid + 1;
                 }
             }
-            else //right half is sorted eg: [5,0,1,2,3]
+            else
             {
-                if(nums[mid] < target && target <= nums[high]) //target is in right sorted half
+                //right half is sorted, check if target is within right half
+                if(nums[mid] < target && target <= nums[high])
                 {
                     low = mid + 1;
                 }
                 else
                 {
-                    high = mid - 1; //go to left half which is a smaller rotated sorted array
+                    high = mid - 1;
                 }
             }
         }
-        
-        return false; //not found
+
+        return false; //target absent
     }
 }
