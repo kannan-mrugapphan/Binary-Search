9c153ea413c89568b8f39038ed8959bf91423eae
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-08-13 21:55:55 -0500

    Update SplitArrayLargestSum.java

--- SplitArrayLargestSum.java
+++ SplitArrayLargestSum.java
@@ -1,69 +1,68 @@
 // 410.
-// time - O(n * log[sum of all elemnts - max element]) with constant space
+// time - O(log(sum(nums[] - max(nums[]))) * length of nums[]) with constant space
 class Solution {
-    public int splitArray(int[] nums, int m) {
+    public int splitArray(int[] nums, int k) {
+        //each element in nums[] should be part of 1 sub array
+        //each subarray should have atleast 1 element
         //edge
-        if(nums == null || nums.length == 0)
+        if(k > nums.length)
         {
-            return 0;
+            return -1; //not possible to form k sub arrays with at least 1 element
         }
-        
-        //observation
-        //largest answer = sum of all elements (m = 1)
-        //smallest answer = max of all elements (m = size of nums)
-        int low = Integer.MIN_VALUE;
-        int high = 0;
+
+        //min sum is each element will be its own sub array with sum = max(nums[])
+        //max sum is all elements are in same sub array with sum = sum(nums[])
+        int low = nums[0]; //should track max in nums[]
+        int high = 0; //should track sum(nums[])
         for(int num : nums)
         {
             low = Math.max(low, num);
             high += num;
         }
-        int result = 0; //return value
+
+        //optimal ans is in [low, high]
+        int result = high; //should be minimized in range, so set it to largest in range
         while(low <= high)
         {
             int mid = low + (high - low) / 2;
-            //for this mid find number of partitions needed
-            int partitions = findNumberOfPartitions(nums, mid);
-            if(partitions > m)
+            //count sub arrays possible if max sum is mid
+            int subarrays = findSubArrays(nums, mid);
+
+            if(subarrays <= k)
             {
-                //increase target sum to reduce parttions
+                //k sub arrays can be formed
+                result = mid;
+                high = mid - 1; //check if k subarrays can be formed with lower sum, search in left half
+            }
+            else
+            {
+                 //subarrays is more than k, increase max sum so sub arrays can be reduced, search in right half
                 low = mid + 1;
             }
-            // else if(partitions < m)
-            // {
-            //     //reduce target sum to increase number of partitions
-            //     high = mid - 1;
-            // }
-            else
-            {
-                //parttions = m, update result and reduce target sum to find a better answer
-                result = mid;
-                high = mid - 1;
-            }
         }
-        
+
         return result;
     }
-    
-    //util method to find the number of partitions needed such that sum of all elements in any sub array is <= targetSum
-    //time - O(n)
-    private int findNumberOfPartitions(int[] nums, int targetSum) {
-        int partitions = 1;
-        int currentSum = 0;
+
+    //computes number of sub arrys possible if largest sub array sum is given
+    // time - O(n) with constant space
+    private int findSubArrays(int[] nums, int limit)
+    {
+        int count = 1;
+        int currentSum = 0; //current sub array initially doesn't have any elements so sum is 0
         for(int num : nums)
         {
-            if(num + currentSum > targetSum)
+            if(num + currentSum <= limit)
             {
-                //current element can't go in current sub array as sum exceeds target
-                partitions++;
-                currentSum = num;
+                currentSum += num; //current element can be included in current subarray
             }
             else
             {
-                //add current element to current sub array
-                currentSum += num;
+                count++; //current will be part of new sub array
+                currentSum = num;
             }
         }
-        return partitions;
+
+        return count;
     }
 }
