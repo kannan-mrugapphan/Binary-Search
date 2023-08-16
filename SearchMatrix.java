f6738e67766a55f3f434c406872216f900b002fb
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-08-16 14:57:33 -0500

    Update SearchMatrix.java

--- SearchMatrix.java
+++ SearchMatrix.java
@@ -1,32 +1,43 @@
 // 74.
-// time - O(max(m, n))
+// brute force - scan each cell to check if target is present - O(nm) time
+// brute force 2 - for each row in the matrix, check if target is within the row (0th element in row <= target <= last element in row), if true, search for the target in that row through binary serach - O(n) + O(logm) - n time for scanning each row and log m for binary search on selected row
+
+// time - O(log mn) with constant space
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        //edge
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        //initially search space is the whole matrix
+        int low = 0; //lowest cell number is 0
+        int high = (m*n) - 1; //last cell number 
+
+        while(low <= high)
         {
-            return false;
-        }
-        
-        //start from top right cell
-        int row = 0;
-        int col = matrix[0].length - 1;
-        //as long as there are more rows and cols
-        while(row < matrix.length && col >= 0)
-        {
+            //as long as search space is valid
+            int mid = low + (high - low) / 2;
+            //get the row, col for mid
+            int row = mid / m;
+            int col = mid % m;
+            //check if mid cell is target
             if(matrix[row][col] == target)
             {
-                return true; //found
+                return true;
             }
-            else if(matrix[row][col] < target) //all elements in current row are smaller than target
+
+            else if(matrix[row][col] < target)
             {
-                row++;
+                //all cells to left of mid (including mid) is small
+                low = mid + 1; //search in right half
             }
-            else if(matrix[row][col] > target) //all elements in current col are greater than target
+
+            else if(matrix[row][col] > target)
             {
-                col--;
+                //all cells to right of mid (including mid) is large
+                high = mid - 1; //serach in left half
             }
         }
-        return false; //not found
+
+        return false;
     }
 }
